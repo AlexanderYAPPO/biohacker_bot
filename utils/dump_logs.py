b947e3203bcffafe8d6150946768996ba48c99ad
Sasha Aleksiiants <aleksiyantsa@gmail.com>
2020-08-10 21:44:49 +0200

    add a script for dumping logs from the DB

---
+++ utils/dump_logs.py
@@ -0,0 +1,43 @@
+import argparse
+import datetime
+import json
+
+from mongoclient import get_logs_collection
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Dump user logs from the DB')
+    parser.add_argument("start_date", type=str, help="start date of the period to dump (this day included, format YYYY-MM-DD)")
+    parser.add_argument("end_date", type=str, help="end date of the period to dump (this day included, format YYYY-MM-DD)")
+    parser.add_argument("--users", action="store", default='')
+
+    args = parser.parse_args()
+
+    start_time = datetime.datetime.strptime(args.start_date, '%Y-%m-%d')
+    end_time = datetime.datetime.strptime(args.end_date, '%Y-%m-%d')
+    users = args.users.split(',') if args.users else None
+
+    if start_time > end_time:
+        raise ValueError('start_time > end_time')
+
+    log_coll = get_logs_collection()
+    q = {
+        'date': {
+            '$gte': start_time,
+            '$lte': end_time,
+        },
+    }
+    if users is not None:
+        q['username'] = {
+            '$in': users
+        }
+
+    result = []
+
+    cursor = log_coll.find(q)
+    for rec in cursor:
+        result.append(rec)
+
+    with open('dump_%s_%s.txt' % (args.start_date, args.end_date), 'w') as wf:
+        for d in result:
+            wf.write(json.dumps(d) + '\n')
