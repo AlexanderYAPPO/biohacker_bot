f855840f15ada10d666624ab49d1690caf5f547b
Sasha Aleksiiants <aleksiyantsa@gmail.com>
2020-08-29 16:01:02 +0200

    fix dump_logs script, add an sh script for dumping all data

---
+++ biohack_questions/utils/dump_everything.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 utils/dump_logs.py 2020-01-01 2022-01-01
+tar -cvf ~/dump_images.tar -C ~ biohack_downloads
+tar -cvf ~/dump_results.tar -C ~ dump_2020-01-01_2022-01-01.txt dump_images.tar
--- biohack_questions/utils/dump_logs.py
+++ biohack_questions/utils/dump_logs.py
@@ -1,6 +1,7 @@
 import argparse
 import datetime
 import json
+from pathlib import Path
 
 from biohack_questions.src.mongoclient import get_logs_collection
 
@@ -36,8 +37,10 @@ if __name__ == '__main__':
 
     cursor = log_coll.find(q)
     for rec in cursor:
+        rec['_id'] = str(rec['_id'])
+        rec['date'] = str(rec['date'])
         result.append(rec)
 
-    with open('dump_%s_%s.txt' % (args.start_date, args.end_date), 'w') as wf:
+    with open('%s/dump_%s_%s.txt' % (str(Path.home()), args.start_date, args.end_date), 'w') as wf:
         for d in result:
             wf.write(json.dumps(d) + '\n')
